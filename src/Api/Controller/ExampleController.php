e50d1cd9b9d0c44d5ecdf192ef88af7349c5cfb2
Jakub Kisielewski <kisielewski.kb@gmail.com>
2018-12-23 15:20:47 +0100

    test sonarcloud

--- src/Api/Controller/ExampleController.php
+++ src/Api/Controller/ExampleController.php
@@ -16,6 +16,7 @@ class ExampleController extends AbstractController
      */
     public function __invoke(ExampleDto $dto)
     {
+        $unused='nonPSR2Syntax';
         return new JsonResponse('ok');
     }
 }
